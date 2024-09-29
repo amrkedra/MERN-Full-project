5db8efed227ea8fd6bae65b4b177d28a05183b14
amrkedra <amrkedra.1993@gmail.com>
2024-09-29 11:47:08 +0300

    fixed the infra pipeline

--- Terraform/providers.tf
+++ Terraform/providers.tf
@@ -30,13 +30,13 @@ terraform {
   }
 }
 
-provider "azurerm" {
-  features {}
-  subscription_id = var.azure_subscription_id
-  tenant_id       = var.azure_tenant_id
-  client_id       = var.azure_client_id
-  client_secret   = var.azure_client_secret
-}
+# provider "azurerm" {
+#   features {}
+#   subscription_id = var.azure_subscription_id
+#   tenant_id       = var.azure_tenant_id
+#   client_id       = var.azure_client_id
+#   client_secret   = var.azure_client_secret
+# }
 
 provider "aws" {
   region  = var.aws_Region
